b8c9cd15303057e670dbb5e1996c0debd0646e16
sans-byte <sanskarj15@gmail.com>
2025-04-19 03:05:56 +0530

    header footer fix response

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export const Footer: React.FC = () => {
   return (
     <>
-      <footer className="h-10 w-full flex bg-red-400">
-        <nav className="w-full flex items-center justify-around p-3 pt-3 text-white">
+      <footer className="flex w-full h-10 justify-center items-center px-3 bg-red-400">
+        <nav className="w-full md:w-3/4 lg:w-1/3 flex justify-between items-center text-white">
           <button className="flex flex-col items-center p-2">
             <Home size={20} />
             <span className="text-xs mt-1"></span>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,34 +5,37 @@ import avatar from "../assets/avatar.jpg";
 export const Header: React.FC = () => {
   return (
     <>
-      <div className="flex w-full h-14 shadow-md justify-between items-center px-3">
-        <section>
-          <img
-            src="https://shoppin.app/_next/image?url=%2Fassets%2Fimages%2Flogo.webp&w=256&q=75"
-            height={100}
-            width={100}
-            alt="Logo"
-          />
-        </section>
-        <section className="flex justify-center items-center gap-2">
-          <input
-            type="search"
-            name=""
-            id=""
-            placeholder="animated string"
-            className="border-[0.5px] rounded-full border-secondary-grey pl-4 p-2 box-content font-comfortaa text-xs focus:outline-none"
-          />
-          <button>
-            <Search />
-          </button>
-          <button>
-            <Heart />
-          </button>
-          <button className="rounded-full flex justify-center items-center h-8 w-8 border-[0.5px] overflow-hidden">
-            <img src={avatar} alt="avatar" />
-          </button>
-        </section>
-      </div>
+      <header className="flex w-full h-14 shadow-md justify-center items-center px-3">
+        <div className="w-full md:w-3/4 lg:w-1/3 flex justify-between items-center">
+          <section className="">
+            <img
+              src="https://shoppin.app/_next/image?url=%2Fassets%2Fimages%2Flogo.webp&w=256&q=75"
+              height={100}
+              width={100}
+              className=""
+              alt="Logo"
+            />
+          </section>
+          <section className="flex justify-center items-center gap-2">
+            <input
+              type="search"
+              name=""
+              id=""
+              placeholder="animated"
+              className="max-sm:w-1/3 border-[0.5px] rounded-full border-secondary-grey pl-4 p-2 box-content font-comfortaa text-xs focus:outline-none"
+            />
+            <button>
+              <Search />
+            </button>
+            <button>
+              <Heart />
+            </button>
+            <button className="rounded-full flex justify-center items-center h-8 w-8 border-[0.5px] overflow-hidden">
+              <img src={avatar} alt="avatar" />
+            </button>
+          </section>
+        </div>
+      </header>
     </>
   );
 };
